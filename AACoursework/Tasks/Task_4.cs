0166733980ef9ad22244e92e43a527111567603d
Seymour <Seymour>
2018-10-19 04:21:53 +0300

    Refactor task # 4

--- AACoursework/Tasks/Task_4.cs
+++ AACoursework/Tasks/Task_4.cs
@@ -9,85 +9,30 @@ namespace AACoursework.Tasks
 {
     public static class Task_4
     {
-        private static void AlgorithmH(int n, int m, ConcurrentQueue<string> queue)
+        private static void AlgorithmH(int n, int m, ConcurrentQueue<string> queue, bool modifiedVersion)
         {
             var a = new int[m + 1];
             var x = 0;
 
             H1:
-            a[0] = n - m + 1;
-            for (int j1 = 1; j1 < m; j1++) //!
+            if (modifiedVersion)
             {
-                a[j1] = 1;
-            }
-            a[m] = -1;
-
-            H2:
-            var currentResult = string.Join("", a.Take(a.Length - 1));
-            queue.Enqueue(currentResult);
-            var a2 = a[1];
-            var a1 = a[0];
-            if (a2 >= a1 - 1)
-            {
-                goto H4;
-            }
-
-            H3:
-            a[0] = a[0] - 1;
-            a[1] = a[1] + 1;
-            goto H2;
-
-            H4:
-            var j = 2;
-            var s = a[0] + a[1] - 1;
-
-            if (a[j] >= a[0] - 1)
-            {
-                do
+                a[0] = n;
+                for (int j1 = 1; j1 < m; j1++) //!
                 {
-                    s = s + a[j];
-                    j = j + 1;
+                    a[j1] = 0;
                 }
-                while (a[j] >= a[0] - 1);
-            }
-
-            H5:
-
-            if ((j + 1) > m)
-            {
-                return;
+                a[m] = -1;
             }
             else
             {
-                x = a[j] + 1;
-                a[j] = x;
-                j = j - 1;
+                a[0] = n - m + 1;
+                for (int j1 = 1; j1 < m; j1++) //!
+                {
+                    a[j1] = 1;
+                }
+                a[m] = -1;
             }
-
-            H6:
-            while (j > 0)
-            {
-                a[j] = x;
-                s = s - x;
-                j = j - 1;
-            }
-
-            a[0] = s;
-            goto H2;
-        }
-
-        private static void AlgorithmHModified(int n, int m, ConcurrentQueue<string> queue)
-        {
-            var a = new int[m + 1];
-            var x = 0;
-
-            H1:
-            a[0] = n;
-            for (int j1 = 1; j1 < m; j1++) //!
-            {
-                a[j1] = 0;
-            }
-            a[m] = -1;
 
             H2:
             var currentResult = string.Join("", a.Take(a.Length - 1));
@@ -152,14 +97,7 @@ namespace AACoursework.Tasks
             {
                 try
                 {
-                    if (useModifiedVersion)
-                    {
-                        AlgorithmHModified(n, m, queue);
-                    }
-                    else
-                    {
-                        AlgorithmH(n, m, queue);
-                    }
+                    AlgorithmH(n, m, queue, useModifiedVersion);
                 }
                 catch (Exception ex)
                 {
